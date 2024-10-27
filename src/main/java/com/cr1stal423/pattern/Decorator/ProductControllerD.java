7550e98d6303e74b71e5e98e0b07b5372ac6378c
whatIsLove <vlad.galyara@gmail.com>
2024-10-27 18:40:42 +0200

    Update Decorator

--- src/main/java/com/cr1stal423/pattern/Decorator/ProductControllerD.java
+++ src/main/java/com/cr1stal423/pattern/Decorator/ProductControllerD.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProductController {
+public class ProductControllerD {
     private final IProductService productService;
 
     @Autowired
-    public ProductController(@Qualifier("productServiceDecorator")IProductService productService) {
+    public ProductControllerD(@Qualifier("productServiceDecorator")IProductService productService) {
         this.productService = productService;
     }
 
